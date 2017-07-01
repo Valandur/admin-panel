5653cab0fe59d8cc6cf1f993fc0f5e4db75b9a64
Marco Crespi <mrc@flxlabs.com>
2017-07-01 21:10:26 +0200

    feat(dashboard): Add TPS and player graphs

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,8 +1,85 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux"
-import { Row, Col, Card, CardBlock } from "reactstrap"
+import { Row, Col, Card, CardHeader, CardBlock } from "reactstrap"
+import { Line } from "react-chartjs-2";
+import _ from "lodash"
 
 import { requestInfo } from "../../actions"
+
+const line = {
+	datasets: [
+		{
+			label: 'Average TPS',
+			fill: false,
+			backgroundColor: 'rgb(255, 99, 132)',
+			borderColor: 'rgb(255, 99, 132)',
+			pointRadius: 0,
+			pointHitRadius: 10,
+			xAxisID: "time",
+			yAxisID: "tps",
+		},
+		{
+			label: 'Online players',
+			fill: false,
+			backgroundColor: 'rgb(54, 162, 235)',
+			borderColor: 'rgb(54, 162, 235)',
+			pointRadius: 0,
+			pointHitRadius: 10,
+			xAxisID: "time",
+			yAxisID: "players",
+		}
+	]
+};
+
+const options = {
+	maintainAspectRatio: false,
+	legend: {
+		display: false,
+	},
+	scales: {
+		xAxes: [{
+			id: "time",
+			type: "time",
+			time: {
+				displayFormats: {
+						second: "HH:mm:ss",
+						minute: "HH:mm",
+						hour: "HH:mm",
+						day: "DD.MM.YYYY",
+				},
+				tooltipFormat: "DD.MM.YYYY HH:mm:ss"
+			}
+		}],
+		yAxes: [{
+			type: "linear",
+			id: "tps",
+			ticks: {
+				beginAtZero: true,
+				max: 20,
+				min: 0,
+			},
+			scaleLabel: {
+				display: true,
+				labelString: "TPS",
+			},
+		},{
+			type: "linear",
+			id: "players",
+			gridLines: {
+				drawOnChartArea: false,
+			},
+			ticks: {
+				beginAtZero: true,
+				min: 0,
+			},
+			scaleLabel: {
+				display: true,
+				labelString: "Players",
+			},
+			position: "right",
+		}]
+	},
+};
 
 class Dashboard extends Component {
 
@@ -10,6 +87,8 @@ class Dashboard extends Component {
 		super(props);
 
 		this.props.requestInfo();
+
+		this.test = this.test.bind(this);
 	}
 
 	componentDidMount() {
@@ -20,7 +99,18 @@ class Dashboard extends Component {
 		clearInterval(this.interval);
 	}
 
+	test() {
+		this.props.test();
+	}
+
 	render() {
+		if (!this.props.data)
+			return null;
+
+		line.labels = _.map(this.props.data.averageTps, tps => new Date(tps.first * 1000))
+		line.datasets[0].data = _.map(this.props.data.averageTps, tps => tps.second)
+		line.datasets[1].data = _.map(this.props.data.onlinePlayers, pls => pls.second)
+
 		let playerState = "primary";
 		if (this.props.data) {
 			const ratio = this.props.data.players / this.props.data.maxPlayers;
@@ -44,83 +134,97 @@ class Dashboard extends Component {
 
 		return (
 			<div className="animated fadeIn">
-				{ this.props.data ?
-					<Row>
+				<Row>
 
-						<Col xs={6} lg={3}>
-							<Card inverse={true} color={playerState}>
-								<CardBlock className="pb-0">
-									<h4 className="mb-0">{this.props.data.players}/{this.props.data.maxPlayers}</h4>
-									<p>Online players</p>
-								</CardBlock>
-							</Card>
-						</Col>
+					<Col xs={6} lg={3}>
+						<Card inverse={true} color={playerState}>
+							<CardBlock className="pb-0">
+								<h4 className="mb-0">{this.props.data.players}/{this.props.data.maxPlayers}</h4>
+								<p>Online players</p>
+							</CardBlock>
+						</Card>
+					</Col>
 
-						<Col xs={6} lg={3}>
-							<Card inverse={true} color={tpsState}>
-								<CardBlock className="pb-0">
-									<h4 className="mb-0">{this.props.data.tps}</h4>
-									<p>Current TPS</p>
-								</CardBlock>
-							</Card>
-						</Col>
+					<Col xs={6} lg={3}>
+						<Card inverse={true} color={tpsState}>
+							<CardBlock className="pb-0">
+								<h4 className="mb-0">{this.props.data.tps}</h4>
+								<p>Current TPS</p>
+							</CardBlock>
+						</Card>
+					</Col>
 
-						<Col xs={6} lg={3}>
-							<Card inverse={true} color="info">
-								<CardBlock className="pb-0">
-									<h4 className="mb-0">{this.props.data.address}</h4>
-									<p>Address</p>
-								</CardBlock>
-							</Card>
-						</Col>
+					<Col xs={6} lg={3}>
+						<Card inverse={true} color="info">
+							<CardBlock className="pb-0">
+								<h4 className="mb-0">{this.props.data.address}</h4>
+								<p>Address</p>
+							</CardBlock>
+						</Card>
+					</Col>
 
-						<Col xs={6} lg={3}>
-							<Card inverse={true} color="info">
-								<CardBlock className="pb-0">
-									<h4 className="mb-0">{this.props.data.onlineMode ? "Yes" : "No"}</h4>
-									<p>Online mode</p>
-								</CardBlock>
-							</Card>
-						</Col>
+					<Col xs={6} lg={3}>
+						<Card inverse={true} color="info">
+							<CardBlock className="pb-0">
+								<h4 className="mb-0">{this.props.data.onlineMode ? "Yes" : "No"}</h4>
+								<p>Online mode</p>
+							</CardBlock>
+						</Card>
+					</Col>
 
-						<Col xs={6} lg={3}>
-							<Card inverse={true} color="primary">
-								<CardBlock className="pb-0">
-									<h4 className="mb-0">{this.props.data.uptimeTicks}</h4>
-									<p>Uptime ticks</p>
-								</CardBlock>
-							</Card>
-						</Col>
+					<Col xs={6} lg={3}>
+						<Card inverse={true} color="primary">
+							<CardBlock className="pb-0">
+								<h4 className="mb-0">{this.props.data.uptimeTicks}</h4>
+								<p>Uptime ticks</p>
+							</CardBlock>
+						</Card>
+					</Col>
 
-						<Col xs={6} lg={3}>
-							<Card inverse={true} color="primary">
-								<CardBlock className="pb-0">
-									<h4 className="mb-0">{this.props.data.game.version}</h4>
-									<p>Minecraft Version</p>
-								</CardBlock>
-							</Card>
-						</Col>
+					<Col xs={6} lg={3}>
+						<Card inverse={true} color="primary">
+							<CardBlock className="pb-0">
+								<h4 className="mb-0">{this.props.data.game.version}</h4>
+								<p>Minecraft Version</p>
+							</CardBlock>
+						</Card>
+					</Col>
 
-						<Col xs={6} lg={3}>
-							<Card inverse={true} color="primary">
-								<CardBlock className="pb-0">
-									<h4 className="mb-0">{this.props.data.api.version}</h4>
-									<p>API Version</p>
-								</CardBlock>
-							</Card>
-						</Col>
+					<Col xs={6} lg={3}>
+						<Card inverse={true} color="primary">
+							<CardBlock className="pb-0">
+								<h4 className="mb-0">{this.props.data.api.version}</h4>
+								<p>API Version</p>
+							</CardBlock>
+						</Card>
+					</Col>
 
-						<Col xs={6} lg={3}>
-							<Card inverse={true} color="primary">
-								<CardBlock className="pb-0">
-									<h4 className="mb-0">{this.props.data.implementation.version}</h4>
-									<p>Implementation Version</p>
-								</CardBlock>
-							</Card>
-						</Col>
+					<Col xs={6} lg={3}>
+						<Card inverse={true} color="primary">
+							<CardBlock className="pb-0">
+								<h4 className="mb-0">{this.props.data.implementation.version}</h4>
+								<p>Implementation Version</p>
+							</CardBlock>
+						</Card>
+					</Col>
 
-					</Row>
-				: null}
+				</Row>
+				<Row>
+
+					<Col xs={12}>
+						<Card>
+							<CardHeader>
+								Average TPS
+							</CardHeader>
+							<CardBlock>
+								<div className="chart-wrapper">
+									<Line data={line} options={options} />
+								</div>
+							</CardBlock>
+						</Card>
+					</Col>
+
+				</Row>
 			</div>
 		)
 	}
@@ -138,7 +242,7 @@ const mapDispatchToProps = (dispatch) => {
 	return {
 		requestInfo: () => {
 			dispatch(requestInfo())
-		}
+		},
 	}
 }
 
