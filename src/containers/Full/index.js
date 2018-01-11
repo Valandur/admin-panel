271bbeebcc4ee3270ae980722071684b38ee14a1
Valandur <inithilian@gmail.com>
2018-01-11 21:46:54 +0100

    feat(menu): Improve menu layout and add links to external resources

--- src/containers/Full/index.js
+++ src/containers/Full/index.js
@@ -38,26 +38,49 @@ const HuskyCrates = Loadable({ loader: () => import("../Integrations/HuskyCrates
 const WebBooks = Loadable({ loader: () => import("../Integrations/WebBooks"), loading: Loading })
 const MMCTickets = Loadable({ loader: () => import("../Integrations/MMCTickets"), loading: Loading })
 
+const apiLink = "/docs"
+const spongeLink = "https://forums.spongepowered.org/t/web-api-v4-adminpanel-restful-web-server-now-with-screenshots/15717"
+const docsLink = "https://github.com/Valandur/Web-API/blob/master/docs/INDEX.md"
+const issuesLink = "https://github.com/Valandur/Web-API/issues"
 
 class Full extends Component {
+
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			show: true
+		}
+
+		this.toggleSidebar = this.toggleSidebar.bind(this)
+	}
 	
 	componentDidMount() {
 		this.props.requestServlets();
 	}
 
+	toggleSidebar() {
+		this.setState({
+			show: !this.state.show
+		})
+	}
+
 	render() {
 		const _t = this.props.t
 
-		return <Sidebar.Pushable style={{ minHeight: "100vh" }}>
-			<Sidebar as={Menu} visible={true} vertical secondary style={{ width: "220px" }}>
-
-				<Menu.Item name="logo" header as={NavLink} to="/">
+		return <div>
+			<Menu fluid stackable size="small" style={{ marginBottom: 0 }}>
+				<Menu.Item as="a" header style={{ minWidth: "259px" }}>
 					<Image size="small" centered src="./img/logo.png" />
 				</Menu.Item>
 
-				<Menu.Item name="settings">
+				<Menu.Item name="logout" onClick={this.toggleSidebar}>
+					<Icon name="sidebar" size="large" />
+				</Menu.Item>
+
+				<Menu.Item>
 					<Dropdown
-						fluid selection
+						selection
 						placeholder="Change language"
 						options={[{
 							text: "English",
@@ -65,136 +88,177 @@ class Full extends Component {
 						},{
 							text: "Deutsch",
 							value: "de",
+						},{
+							"text": "русский",
+							value: "ru",
 						}]}
 						value={this.props.lang}
 						onChange={(e, data) => this.props.changeLanguage(data.value)}
 					/>
 				</Menu.Item>
 
-				<Menu.Item name="dashboard" as={NavLink} to="/dashboard">
-					<Icon name="dashboard" /> {_t("Dashboard")}
-				</Menu.Item>
+				<Menu.Menu position="right">
+					<Menu.Item>
+						<a href={apiLink} target="_blank" rel="noopener noreferrer">
+							<Icon name="external" />{_t("APILink")}
+						</a>
+					</Menu.Item>
+					<Menu.Item>
+						<a href={spongeLink} target="_blank" rel="noopener noreferrer">
+							<Icon name="external" />{_t("SpongeLink")}
+						</a>
+					</Menu.Item>
+					<Menu.Item>
+						<a href={docsLink} target="_blank" rel="noopener noreferrer">
+							<Icon name="external" />{_t("DocsLink")}
+						</a>
+					</Menu.Item>
+					<Menu.Item>
+						<a href={issuesLink} target="_blank" rel="noopener noreferrer">
+							<Icon name="external" />{_t("IssuesLink")}
+						</a>
+					</Menu.Item>
 
-				<Menu.Item name="chat" as={NavLink} to="/chat">
-					<Icon name="comments" /> {_t("Chat")}
-				</Menu.Item>
+					<Menu.Item name="logout" onClick={this.props.requestLogout}>
+						<Icon name="log out" /> {_t("Logout")}
+					</Menu.Item>
+				</Menu.Menu>
+			</Menu>
 
-				<Menu.Item name="commands" as={NavLink} to="/commands">
-					<Icon name="terminal" /> {_t("Commands")}
-				</Menu.Item>
+			<Sidebar.Pushable style={{ height: "calc(100vh - 65px)" }}>
+				<Sidebar
+						vertical
+						as={Menu}
+						animation="push"
+						visible={this.state.show}>
 
-				<Menu.Item name="map" as={NavLink} to="/map">
-					<Icon name="map" /> {_t("Map")}
-				</Menu.Item>
+					<Menu.Item name="dashboard" as={NavLink} to="/dashboard">
+						<Icon name="dashboard" /> {_t("Dashboard")}
+					</Menu.Item>
 
-				<Menu.Item name="worlds" as={NavLink} to="/worlds">
-					<Icon name="globe" /> {_t("Worlds")}
-				</Menu.Item>
+					<Menu.Item name="chat" as={NavLink} to="/chat">
+						<Icon name="comments" /> {_t("Chat")}
+					</Menu.Item>
 
-				<Menu.Item name="players" as={NavLink} to="/players">
-					<Icon name="users" /> {_t("Players")}
-				</Menu.Item>
+					<Menu.Item name="commands" as={NavLink} to="/commands">
+						<Icon name="terminal" /> {_t("Commands")}
+					</Menu.Item>
 
-				<Menu.Item name="entities" as={NavLink} to="/entities">
-					<Icon name="paw" /> {_t("Entities")}
-				</Menu.Item>
+					<Menu.Item name="map" as={NavLink} to="/map">
+						<Icon name="map" /> {_t("Map")}
+					</Menu.Item>
 
-				<Menu.Item name="tile-entities" as={NavLink} to="/tile-entities">
-					<Icon name="puzzle" /> {_t("TileEntities")}
-				</Menu.Item>
+					<Menu.Item name="worlds" as={NavLink} to="/worlds">
+						<Icon name="globe" /> {_t("Worlds")}
+					</Menu.Item>
 
-				<Menu.Item name="block-operations" as={NavLink} to="/block-operations">
-					<Icon className="fa-th-large" /> {_t("BlockOperations")}
-				</Menu.Item>
+					<Menu.Item name="players" as={NavLink} to="/players">
+						<Icon name="users" /> {_t("Players")}
+					</Menu.Item>
 
-				<Menu.Item name="plugins" as={NavLink} to="/plugins">
-					<Icon name="plug" /> {_t("Plugins")}
-				</Menu.Item>
+					<Menu.Item name="entities" as={NavLink} to="/entities">
+						<Icon name="paw" /> {_t("Entities")}
+					</Menu.Item>
 
-				<Menu.Item name="server-settings" as={NavLink} to="/server-settings">
-					<Icon name="cogs" /> {_t("ServerSettings")}
-				</Menu.Item>
+					<Menu.Item name="tile-entities" as={NavLink} to="/tile-entities">
+						<Icon name="puzzle" /> {_t("TileEntities")}
+					</Menu.Item>
 
-				{ this.props.servlets.husky &&
-				<Menu.Item>
-					<Menu.Header>{_t("HuskyCrates")}</Menu.Header>
+					<Menu.Item name="block-operations" as={NavLink} to="/block-operations">
+						<Icon className="fa-th-large" /> {_t("BlockOperations")}
+					</Menu.Item>
 
-					<Menu.Menu>
-						<Menu.Item name="husky-crates" as={NavLink} to="/husky/crates">
-							<Icon name="archive" /> {_t("Crates")}
-						</Menu.Item>
-					</Menu.Menu>
-				</Menu.Item>
-				}
+					<Menu.Item name="plugins" as={NavLink} to="/plugins">
+						<Icon name="plug" /> {_t("Plugins")}
+					</Menu.Item>
 
-				{ this.props.servlets.mmctickets &&
-				<Menu.Item>
-					<Menu.Header>{_t("MMCTickets")}</Menu.Header>
+					<Menu.Item name="server-settings" as={NavLink} to="/server-settings">
+						<Icon name="cogs" /> {_t("ServerSettings")}
+					</Menu.Item>
 
-					<Menu.Menu>
-						<Menu.Item name="mmc-tickets" as={NavLink} to="/mmctickets/tickets">
-							<Icon name="ticket" /> {_t("Tickets")}
-						</Menu.Item>
-					</Menu.Menu>
-				</Menu.Item>
-				}
+					{ this.props.servlets.husky &&
+					<Menu.Item>
+						<Menu.Header>{_t("HuskyCrates")}</Menu.Header>
 
-				{ this.props.servlets.nucleus &&
-				<Menu.Item>
-					<Menu.Header>{_t("Nucleus")}</Menu.Header>
+						<Menu.Menu>
+							<Menu.Item name="husky-crates" as={NavLink} to="/husky/crates">
+								<Icon name="archive" /> {_t("Crates")}
+							</Menu.Item>
+						</Menu.Menu>
+					</Menu.Item>
+					}
 
-					<Menu.Menu>
-						<Menu.Item name="nucleus-kits" as={NavLink} to="/nucleus/kits">
-							<Icon name="wrench" /> {_t("Kits")}
-						</Menu.Item>
+					{ this.props.servlets.mmctickets &&
+					<Menu.Item>
+						<Menu.Header>{_t("MMCTickets")}</Menu.Header>
 
-						<Menu.Item name="nucleus-jails" as={NavLink} to="/nucleus/jails">
-							<Icon name="bars" rotated="clockwise" /> {_t("Jails")}
-						</Menu.Item>
-					</Menu.Menu>
-				</Menu.Item>
-				}
+						<Menu.Menu>
+							<Menu.Item name="mmc-tickets" as={NavLink} to="/mmctickets/tickets">
+								<Icon name="ticket" /> {_t("Tickets")}
+							</Menu.Item>
+						</Menu.Menu>
+					</Menu.Item>
+					}
 
-				{ this.props.servlets.webbooks &&
-				<Menu.Item>
-					<Menu.Header>{_t("WebBooks")}</Menu.Header>
+					{ this.props.servlets.nucleus &&
+					<Menu.Item>
+						<Menu.Header>{_t("Nucleus")}</Menu.Header>
 
-					<Menu.Menu>
-						<Menu.Item name="web-books" as={NavLink} to="/webbooks/books">
-							<Icon name="book" /> {_t("Books")}
-						</Menu.Item>
-					</Menu.Menu>
-				</Menu.Item>
-				}
+						<Menu.Menu>
+							<Menu.Item name="nucleus-kits" as={NavLink} to="/nucleus/kits">
+								<Icon name="wrench" /> {_t("Kits")}
+							</Menu.Item>
 
-				<Menu.Item name="logout" onClick={this.props.requestLogout}>
-					<Icon name="log out" /> {_t("Logout")}
-				</Menu.Item>
-			</Sidebar>
+							<Menu.Item name="nucleus-jails" as={NavLink} to="/nucleus/jails">
+								<Icon name="bars" rotated="clockwise" /> {_t("Jails")}
+							</Menu.Item>
+						</Menu.Menu>
+					</Menu.Item>
+					}
 
-			<Sidebar.Pusher style={{ width: "calc(100% - 220px)", height: "100vh", overflowY: "scroll" }}>
-				<Switch>
-					<Route path="/dashboard" name="Dashboard" component={Dashboard} />
-					<Route path="/chat" name="Chat" component={Chat} />
-					<Route path="/commands" name="Commands" component={Commands} />
-					<Route path="/map" name="Map" component={Map} />
-					<Route path="/worlds" name="Worlds" component={Worlds} />
-					<Route path="/players" name="Players" component={Players} />
-					<Route path="/entities" name="Entities" component={Entities} />
-					<Route path="/tile-entities" name="Tile Entities" component={TileEntities} />
-					<Route path="/block-operations" name="Block Operations" component={BlockOperations} />
-					<Route path="/plugins" name="Plugins" component={Plugins} />
-					<Route path="/server-settings" name="Server Settings" component={ServerSettings} />
+					{ this.props.servlets.webbooks &&
+					<Menu.Item>
+						<Menu.Header>{_t("WebBooks")}</Menu.Header>
 
-					<Route path="/husky" name="HuskyCrates" component={HuskyCrates} />
-					<Route path="/mmctickets" name="MMCTickets" component={MMCTickets} />
-					<Route path="/nucleus" name="Nucleus" component={Nucleus} />
-					<Route path="/webbooks" name="WebBooks" component={WebBooks} />
-					
-					<Redirect from="/" to="/dashboard" />
-				</Switch>
-			</Sidebar.Pusher>
-		</Sidebar.Pushable>
+						<Menu.Menu>
+							<Menu.Item name="web-books" as={NavLink} to="/webbooks/books">
+								<Icon name="book" /> {_t("Books")}
+							</Menu.Item>
+						</Menu.Menu>
+					</Menu.Item>
+					}
+				</Sidebar>
+
+				<Sidebar.Pusher
+						style={{
+							width: this.state.show ? "calc(100% - 260px)" : "calc(100%)",
+							transition: "width 0.5s",
+							height: "100%",
+							overflowY: "scroll",
+						}}>
+					<Switch>
+						<Route path="/dashboard" name="Dashboard" component={Dashboard} />
+						<Route path="/chat" name="Chat" component={Chat} />
+						<Route path="/commands" name="Commands" component={Commands} />
+						<Route path="/map" name="Map" component={Map} />
+						<Route path="/worlds" name="Worlds" component={Worlds} />
+						<Route path="/players" name="Players" component={Players} />
+						<Route path="/entities" name="Entities" component={Entities} />
+						<Route path="/tile-entities" name="Tile Entities" component={TileEntities} />
+						<Route path="/block-operations" name="Block Operations" component={BlockOperations} />
+						<Route path="/plugins" name="Plugins" component={Plugins} />
+						<Route path="/server-settings" name="Server Settings" component={ServerSettings} />
+
+						<Route path="/husky" name="HuskyCrates" component={HuskyCrates} />
+						<Route path="/mmctickets" name="MMCTickets" component={MMCTickets} />
+						<Route path="/nucleus" name="Nucleus" component={Nucleus} />
+						<Route path="/webbooks" name="WebBooks" component={WebBooks} />
+						
+						<Redirect from="/" to="/dashboard" />
+					</Switch>
+				</Sidebar.Pusher>
+			</Sidebar.Pushable>
+		</div>
 	}
 }
 
