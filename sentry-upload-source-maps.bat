4101885735586c0c19bee3d7deadf07be6c7cf37
Valandur <inithilian@gmail.com>
2018-03-23 11:08:39 -0500

    chore(sentry): Fix sentry upload script

--- sentry-upload-source-maps.bat
+++ sentry-upload-source-maps.bat
@@ -1,2 +1,2 @@
 set /p version=Release version (x.x.x)?:
-sentry-cli releases -o webapi -p admin-panel files {version} upload-sourcemaps ./build
+sentry-cli releases -o webapi -p admin-panel files %version% upload-sourcemaps ./build
