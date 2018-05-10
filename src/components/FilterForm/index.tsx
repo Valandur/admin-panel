22af8405991d6a3ddfb74deffeb3a2370683a3ef
Valandur <inithilian@gmail.com>
2018-05-10 12:26:39 +0200

    feat(create & filter): Collapse create & filter forms to save space

--- src/components/FilterForm/index.tsx
+++ src/components/FilterForm/index.tsx
@@ -1,6 +1,6 @@
 import * as _ from "lodash"
 import * as React from "react"
-import { Dropdown, Form, Header, Icon, Message, Segment } from "semantic-ui-react"
+import { Accordion, Dropdown, Form, Icon, Message } from "semantic-ui-react"
 
 import { DataFieldGroup, DataFieldRaw } from "../../types"
 import { handleChange, HandleChangeFunc } from "../Util"
@@ -17,20 +17,36 @@ export interface Props<T> {
 	onFilterChange: (key: string, value: string) => void
 }
 
-class FilterForm<T> extends React.Component<Props<T>> {
+interface State {
+	open: boolean
+}
 
+class FilterForm<T> extends React.Component<Props<T>, State> {
 	handleChange: HandleChangeFunc
 
 	constructor(props: Props<T>) {
 		super(props)
 
+		this.state = {
+			open: false
+		}
+
 		this.handleChange = handleChange.bind(this, this.props.onFilterChange)
 	}
 
-	shouldComponentUpdate(nextProps: Props<T>, nextState: any) {
-		return nextProps.values !== this.props.values ||
+	shouldComponentUpdate(nextProps: Props<T>, nextState: State) {
+		return (
+			nextProps.values !== this.props.values ||
 			nextProps.fields !== this.props.fields ||
-			nextProps.valid !== this.props.valid
+			nextProps.valid !== this.props.valid ||
+			nextState.open !== this.state.open
+		)
+	}
+
+	handleClick() {
+		this.setState({
+			open: !this.state.open
+		})
 	}
 
 	render() {
@@ -41,12 +57,15 @@ class FilterForm<T> extends React.Component<Props<T>> {
 			const field = fields[name]
 			const newField: DataFieldRaw<T> = {
 				...field,
-				name: field.filterName ? field.filterName : name,
+				name: field.filterName ? field.filterName : name
 			}
 
 			if (newField.isGroup) {
 				fieldGroups.push({ only: newField })
-			} else if (fieldGroups.length && !fieldGroups[fieldGroups.length - 1].second) {
+			} else if (
+				fieldGroups.length &&
+				!fieldGroups[fieldGroups.length - 1].second
+			) {
 				fieldGroups[fieldGroups.length - 1].second = newField
 			} else {
 				fieldGroups.push({ first: newField })
@@ -54,44 +73,65 @@ class FilterForm<T> extends React.Component<Props<T>> {
 		})
 
 		return (
-			<Segment>
-				<Header>
+			<Accordion styled fluid>
+				<Accordion.Title
+					active={this.state.open}
+					onClick={() => this.handleClick()}
+				>
 					<Icon name="filter" fitted /> {title}
-				</Header>
+				</Accordion.Title>
 
-				<Form>
-					{fieldGroups.map((fg, i) => {
-						if (fg.only) {
-							return this.renderField(fg.only, _.get(values, fg.only.name), !valid)
-						}
-
-						return <Form.Group key={i} widths="equal">
-							{fg.first &&
-								this.renderField(fg.first, _.get(values, fg.first.name), !valid)
+				<Accordion.Content active={this.state.open}>
+					<Form>
+						{fieldGroups.map((fg, i) => {
+							if (fg.only) {
+								return this.renderField(
+									fg.only,
+									_.get(values, fg.only.name),
+									!valid
+								)
 							}
 
-							{fg.second &&
-								this.renderField(fg.second, _.get(values, fg.second.name), !valid)
-							}
-						</Form.Group>
-					})}
-					<Message
-						error
-						visible={!valid}
-						content="Search term must be a valid regex"
-					/>
-				</Form>
-			</Segment>
+							return (
+								<Form.Group key={i} widths="equal">
+									{fg.first &&
+										this.renderField(
+											fg.first,
+											_.get(values, fg.first.name),
+											!valid
+										)}
+
+									{fg.second &&
+										this.renderField(
+											fg.second,
+											_.get(values, fg.second.name),
+											!valid
+										)}
+								</Form.Group>
+							)
+						})}
+						<Message
+							error
+							visible={!valid}
+							content="Search term must be a valid regex"
+						/>
+					</Form>
+				</Accordion.Content>
+			</Accordion>
 		)
 	}
 
-	renderField(field: DataFieldRaw<T>, value: string | string[], error: boolean) {
+	renderField(
+		field: DataFieldRaw<T>,
+		value: string | string[],
+		error: boolean
+	) {
 		if (typeof field.filter === "function") {
 			return field.filter({
 				state: this.props.values,
 				setState: this.setState,
 				handleChange: this.handleChange,
-				value: value,
+				value: value
 			})
 		}
 
